c8a9008bc3d7de706a7cd258fba4d8c22f90f890
franklinlondono <fdlondono16@gmail.com>
2022-03-17 12:58:36 -0500

    Actualizacion reporsitorio

---
+++ 04/Js/Scripts.js
@@ -0,0 +1,14 @@
+
+
+let usuarioNombre = "franklin de jesus";
+    usuarioApellido ="londoño estrada"
+    nombreCurso = "Bit-Iberoamericana desarrollo full stack"
+const edad = 26      
+
+const concatenar = usuarioNombre + usuarioApellido + nombreCurso +  edad
+
+console.log(concatenar)
+
+console.log( usuarioNombre +" "+ usuarioApellido +" "+ nombreCurso + " "+edad)
+
+
