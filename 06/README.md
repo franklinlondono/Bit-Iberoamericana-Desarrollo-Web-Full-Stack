9e6ef03304966344c07c269e409573570bad1f81
franklinlondono <fdlondono16@gmail.com>
2022-04-07 13:07:19 -0500

    Update

---
+++ 06/README.md
@@ -0,0 +1,3 @@
+# bit-iberoamericana-desarrollo-web-full-stack
+Crear un sitio web con bootstrap, que tenga parte pública y parte privada, controlar acceso a parte privada, subirlo a Netlify y presentarlo en máximo 3 minutos.
+Compartir en Discord la url del código fuente (GitHub) y la url del proyecto desplegado (Netlify) - franklin lonoño 
--- 06/js/main.js
+++ 06/js/main.js
@@ -1,91 +1,89 @@
-
+// estos son objetos simulando backend
 const validCredentials ={
-    username:'fdlondono16@gmail.com',
-    password:'fr@nklin',
-}
+   username: 'fdlondono16@gmail.com',
+   password: 'bit123'
+};
 
-const myCredentials ={
-    username:null,
-    password:null,
 
-}
 
-const username = document.getElementById('inUsername');
-const password = document.getElementById('InPassword');
-const submit = document.getElementById('submitBtn');
+
+// paso 1 crear variables de datos
+
+
+const username = document.getElementById('inUsername')
+const password = document.getElementById('inPassword')
+const submit = document.getElementById('submitBtn')
 const private = document.getElementById('private')
-const modal = document.getElementById('staticBackdrop');
-const close = document.getElementById('closeBtn');
+const modal = document.getElementById('modal')
 const success = document.getElementById('success')
 const error = document.getElementById('error')
+const close = document.getElementById('closeBtn')
 
-
-//funciones
-
-const handleinUsername = (event) =>{
-
-myCredentials.username=event.target.value;
-}
-
-const handleinPassword= (event) =>{
-myCredentials.password=event.target.value;
-
-console.log(myCredentials)
-
+const myCredentials = {
+    username:null,
+    password:null
 };
-const handleSubmitBtn = () => {
-const username = myCredentials.username === validCredentials.username;
-console.log (username)
-const password = myCredentials.password === validCredentials.password;
-console.log (password)
-
- if (username && password){
 
 
- console.log('permitir acceso');
+// paso 3 crear las funciones
+                        // event es un objeto
+                                       // paso 4 capturar el evento
+const handleUsername = (event)=>{
+     console.log('inUsername',event.target.value);
+
+myCredentials.username = event.target.value;  
+    console.log(myCredentials)
+};  
+
+
+const handlePassword =(event)=>{
+    console.log('password')
+myCredentials.password = event.target.value;
+    console.log(myCredentials)
+};
+
+const handleSubmit =(e)=>{
+    e.preventDefault()
+    // al enviar validar si las credenciales son correctas
+const username = myCredentials.username === validCredentials.username
+  console.log(username)
+const password = myCredentials.password === validCredentials.password
+  console.log(password)
+
+if (username && password){
+     console.log('permitir acceso');
+   
 
 private.classList.remove('disabled');
-console.log(private)
+ console.log(private);
+  //console.log(modal)
 
-//console.log(modal);
- console.log(close);
+success.classList.remove('d-none');
+setTimeout(() => {
+success.classList.add('d-none');
+close.click();
+ }, 3000);
 
- //mostrar mensajes de login exitoso
+} else{
+    console.log('denegar acceso');
+error.classList.remove('d-none');
+    // luego, ocultar mensaje de error
+setTimeout(() => {
+error.classList.add('d-none');
+    }, 3000);
+  }
 
- //success.classList.remove('d-none');
-
- //ocultar mensaje de login 
-
- //setTimeout(() => {
- //success.classList.add('d-none');
- //close.click();}, 5000 );
-
-
- }
-else
-
-
-console.log('denegar acceso');
-
- //mostrar error
-
-//error.classList.remove.apply('d-none');
-
- //ocultar mensaje
-
- //setTimeout(() => {
- //error.classList.add('d-none');}, 5000);
 
 }
 
+// paso 2 crear eventos
 
-
-//crear eventos
-
-document.addEventListener('DOMContentLoaded', ()=> {
-    username.addEventListener('input', handleinUsername);
-    password.addEventListener('input', handleinPassword);
-    submit.addEventListener('click' , handleSubmitBtn);
-    
-    
-})
+document.addEventListener('DOMContentLoaded', ()=>{
+//    console.log('listos')         esta es una funcion toca crearla
+username.addEventListener('input', handleUsername);
+                                    // esta es una funcion toca crearla
+password.addEventListener('input', handlePassword);
+                                //  esta funcion toca crearla 
+        // como es boton no es un evento input, es el evento click       
+submit.addEventListener('click',handleSubmit);
+});
